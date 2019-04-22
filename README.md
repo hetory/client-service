bbffb0a1f2f7ccb3ff79e60ea36116d979da2afe
Hetory Ferreira <hetoryferreira@hotmail.com>
2019-04-22 13:05:20 -0300

    alteracao de urlVersion e log

--- README.md
+++ README.md
@@ -2,18 +2,25 @@
 
 JAVA_VERSION : 1.8
 
-Iniciar aplicação
+Iniciar aplicacao
+
+Executar o arquivo na raiz do projeto
+  Executavel : client-service\mvnw.cmd
 
 1 - Start no projeto client-server :
-    Start Class :  Application
+    Start Class :  Application.java
 
 
 
-VERBO : POST
-Content-Type: application/json
+API
+
+URL : 
+
+
+Content-Type:application/json
+Method-Type:POST
 
 BODY :
-
 {  
    "name":"Hetory",
    "limit":1000,
@@ -22,3 +29,33 @@ BODY :
 
 
 
+
+Cenario de testes :
+
+Cenario A
+
+Nome Cliente  : Hetory
+Limite de credito : 10 
+Risco
+Caso A
+ entao retorna taxa de juros = 0
+
+
+
+Cenario B
+
+Nome Cliente  : Hetory
+Limite de credito : 10 
+Risco
+Caso B
+ entao retorna taxa de juros = 10
+
+
+
+Cenario C
+
+Nome Cliente  : Hetory
+Limite de credito : 10 
+Risco
+Caso B
+ entao retorna taxa de juros = 20
--- src/main/java/client/endpoint/ClientEndpoint.java
+++ src/main/java/client/endpoint/ClientEndpoint.java
@@ -4,10 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import client.convert.ClientConverter;
@@ -16,6 +18,7 @@ import client.entity.ClientEntity;
 import client.service.ClientService;
 
 @RestController
+@RequestMapping("/v1")
 public class ClientEndpoint {
 
 	@Autowired
@@ -25,8 +28,8 @@ public class ClientEndpoint {
 		this.clientService = clientService;
 	}
 
-	@CrossOrigin(origins = "http://localhost:4200")
-    @PostMapping("/client")
+	@CrossOrigin
+    @PostMapping(path= "/client" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ClientDto>client(@Valid @RequestBody ClientDto clientDto) {
 	   
 	   ClientEntity client = clientService.create(ClientConverter.dtoToEntity(clientDto));
